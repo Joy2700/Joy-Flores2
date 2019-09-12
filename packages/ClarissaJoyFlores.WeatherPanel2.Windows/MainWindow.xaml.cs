f422fc1476d03e32d5d2d36152bc4be271fe4459
Joy Flores2 <Joy2700>
2019-09-12 02:02:36 -0700

    WeatherPanel2

---
+++ packages/ClarissaJoyFlores.WeatherPanel2.Windows/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RestSharp;
+using Newtonsoft.Json;
+
+
+
+
+namespace ClarissaJoyFlores.WeatherPanel2.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_GetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient("https://api.darksky.net/forecast/64ee9d4e589bb2cb3788596fd477b0f7/14.8781,120.4546");
+
+            var request = new RestRequest("", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            var content = response.Content;
+
+            var area = JsonConvert.DeserializeObject<WeatherArea>(content);
+
+            lblSummary.Content = DateTime.Now.ToString("hh:mm tt");
+            lblSummary.Content = area.Currently.Summary;
+            lblIcon.Content = area.Currently.Icon;
+            lblTemperature.Content = area.Currently.Temperature;
+            lblHumidity.Content = area.Currently.Humidity;
+
+            lblSummary.Content = DateTime.Now.ToString("hh:mm tt");
+
+            BitmapImage WeatherIcon = new BitmapImage();
+            WeatherIcon.BeginInit();
+            WeatherIcon.UriSource = new Uri("C:\\Users\\VGD PC 9\\Desktop\\weather.png");
+            WeatherIcon.EndInit();
+            imgWeather.Source = WeatherIcon;
+
+        }
+    }
+}
---
+++ packages/ClarissaJoyFlores.WeatherPanel2.Windows/WeatherArea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClarissaJoyFlores.WeatherPanel2.Windows
+{
+    class WeatherArea
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+        public CurrentWeather Currently { get; set; }
+    }
+
+    public class CurrentWeather
+    {
+        public string Summary { get; set; }
+
+        public string Icon { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Humidity { get; set; }
+
+        public string Pressure { get; set; }
+
+        public string WindSpeed { get; set; }
+
+
+    }
+}
